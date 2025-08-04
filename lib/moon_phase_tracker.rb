6627757201f22ee798421b3eca36beef5776e69f
Daniel K Lima <dklima@gmail.com>
2025-08-04 01:49:55 -0300

    First commit

---
+++ lib/moon_phase_tracker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "moon_phase_tracker/version"
+
+module MoonPhaseTracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/moon_phase_tracker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MoonPhaseTracker
+  VERSION = "0.1.0"
+end
